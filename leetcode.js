8d539d156877e0492f1f1fe9c2ad713850a99363
Adarsh <adarshsingh2k@gmail.com>
2023-06-28 12:03:22 +0530

    get update 1

--- leetcode.js
+++ leetcode.js
@@ -39,7 +39,7 @@ const query = `
 // format data 
 const formatData = (data) => {
     let sendData =  {
-        profileName:data.matchedUser.username,
+        profileName:data.matchedUser.submitStats.totalSubmissionNum,
         totalSolved: data.matchedUser.submitStats.acSubmissionNum[0].count,
         totalSubmissions:  data.matchedUser.submitStats.totalSubmissionNum,
         totalQuestions: data.allQuestionsCount[0].count,
